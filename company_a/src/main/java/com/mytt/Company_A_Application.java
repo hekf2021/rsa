17e36d9392c58c23e4cc38008bed70c8cb8f07ef
hekunfeng <hekunfeng@traceche.com>
2019-06-06 11:36:29 +0800

    Initial commit

---
+++ company_a/src/main/java/com/mytt/Company_A_Application.java
@@ -0,0 +1,13 @@
+package com.mytt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Company_A_Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Company_A_Application.class, args);
+    }
+
+}
---
+++ company_b/src/main/java/com/mytt/Company_B_Application.java
@@ -0,0 +1,13 @@
+package com.mytt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Company_B_Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Company_B_Application.class, args);
+    }
+
+}
